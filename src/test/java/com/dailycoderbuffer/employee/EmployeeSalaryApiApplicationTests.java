2c8c022b9f4543bcdce1d856cd4a0bf1ebe547b1
JoybitSarkar <jarvis59632@gmail.com>
2023-07-11 23:47:18 +0530

    Setup maven project

---
+++ src/test/java/com/dailycoderbuffer/employee/EmployeeSalaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycoderbuffer.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSalaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
